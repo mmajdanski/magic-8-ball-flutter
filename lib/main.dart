d18c9518e744036b9820966f80c7ac12c94eaf3f
mmajdanski <michael.majdanski@gmail.com>
2020-04-27 14:36:01 -0700

    Final Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNum = 1;
+
+  void newBall() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+      print(ballNum);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () => newBall(),
+              child: Image.asset('images/ball$ballNum.png'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
